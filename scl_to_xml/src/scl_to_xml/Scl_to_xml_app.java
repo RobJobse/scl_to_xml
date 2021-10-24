7de766a608f66335574cecd7ae85a745468f797b
Rob Jobse <robjobse@hetnet.nl>
2021-10-24 09:41:59 +0200

    just comment

---
+++ scl_to_xml/src/scl_to_xml/Scl_to_xml_app.java
@@ -0,0 +1,18 @@
+package scl_to_xml;
+
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Scl_to_xml_app {
+
+	public static void main(String[] args) throws FileNotFoundException,IOException {
+		// TODO Auto-generated method stub
+
+		System.out.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+	
+	    Test test = new Test("LektestOrg");
+
+	}
+
+}
