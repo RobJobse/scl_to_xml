3a45ef10a685c71c715a6200b4447dd1c351713b
Rob Jobse <robjobse@hetnet.nl>
2021-10-25 22:19:15 +0200

    class om voorwaarde en action in op te slaan

---
+++ scl_to_xml/src/scl_to_xml/IfAction.java
@@ -0,0 +1,11 @@
+package scl_to_xml;
+
+public class IfAction {
+	public String voorwaarde;
+	public String actie;
+	
+	public IfAction(String vw,String ac){
+		voorwaarde = vw;
+		actie = ac;
+	}
+}
